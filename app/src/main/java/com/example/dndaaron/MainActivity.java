f2af97ebc0c03a42f367c5a5be1ecd806eb82668
aarricgra <aarricgra@alu.edu.gva.es>
2023-11-10 12:16:17 +0100

    1

--- app/src/main/java/com/example/dndaaron/MainActivity.java
+++ app/src/main/java/com/example/dndaaron/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
